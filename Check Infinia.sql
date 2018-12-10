93d377b8c590a7268ca1fd37036b7e1ce079b6ec
rezarama <39762327+rezarama@users.noreply.github.com>
2018-12-10 19:00:51 +0700

    Delete Check Infinia.sql

--- Check Infinia.sql
+++
@@ -1,8 +0,0 @@
-select o."orderNumber",o."referralCode",max(m.brand)||' '||max(em."egiftName") "itemName"
-from "Orders" o 
-join "OrderEgifts" oe on o.id=oe."OrderId"
-join "Egifts" e on oe."EgiftId"=e.id
-join "EgiftMasters" em on e."EgiftMasterId"=em.id
-join "Merchants" m on e."mId"=m."mId"
---where "referralCode" = '47316'--'txn1527787461'
-where "orderNumber" in

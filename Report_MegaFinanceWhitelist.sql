70fc096afd0f8da62de6351897623c89e9eedaf3
rezarama <39762327+rezarama@users.noreply.github.com>
2018-12-10 19:14:34 +0700

    Delete Report_MegaFinanceWhitelist.sql

--- Report_MegaFinanceWhitelist.sql
+++
@@ -1,5 +0,0 @@
-  select rw.* from  "CardPrograms" cp inner join "RewardPrograms" rp
-  on cp."id"=rp."RewardedCardProgramId"
-  inner join  "RewardWhitelists" rw
-  on rp."id"=rw."RewardProgramId"
-  where cp."MerchantId" in ('320','686','687')
